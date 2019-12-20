19e04401550ccd247fd15a785a6775a8467b01ef
Suhhny <gerang9119@gmail.com>
2019-12-21 04:03:06 +0900

    renewal with server

--- main.js
+++ main.js
@@ -9,14 +9,9 @@ app.set('views', __dirname + '/public');
 
 app.use(express.static(__dirname + '/src'));
 
-app.use(express.static(__dirname + '/src1'));
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://sooohh6:rlatnghkd21@ds259079.mlab.com:59079/whereisjellyfish');
 
 const routes = require('./routes');
 app.use('/', routes);
@@ -24,6 +19,5 @@ app.use('/', routes);
 app.all('*', function(req, res){
   res.status(404).send('404 Error!');
 });
-
 
 app.listen(3000);
--- models/index.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require('mongoose');
-// //
-// // const dateRangeDB = new mongoose.Schema({
-// //   startDate: { type : Date },
-// //   endDate: { type : Date }
-// // });
-// //
-// // module.exports = mongoose.model('index', DateRangeDB); 
